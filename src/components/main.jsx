56d1593fd01c59ede2249487f2c51f261cf45152
shobhith_k <54988109+shobhithk@users.noreply.github.com>
2023-01-28 17:57:55 +0530

    Update main.jsx

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -173,7 +173,7 @@ class Main extends React.Component {
     return (
       <Navbar bg="dark" expand="lg" variant="dark">
                 <Container>
-                  <Navbar.Brand href="">Decrypto 2k22</Navbar.Brand>
+                  <Navbar.Brand href="">Decrypto 2k23</Navbar.Brand>
                   <Navbar.Toggle aria-controls="basic-navbar-nav" />
                   <Navbar.Collapse className="justify-content-end mx-2" id="basic-navbar-nav">
                     <Nav className="">
