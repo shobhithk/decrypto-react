2f8fd95b6303e1626b80dc24a44abb211c13478d
shobhith_k <54988109+shobhithk@users.noreply.github.com>
2023-01-28 11:48:27 +0530

    Update home.jsx

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -14,7 +14,7 @@ class Home extends Component {
             <div>
                 <Navbar bg="dark" expand="lg" variant="dark">
                     <Container>
-                      <Navbar.Brand href="/home">Decrypto 2k22</Navbar.Brand>
+                      <Navbar.Brand href="/home">Decrypto 2k23</Navbar.Brand>
                       <Navbar.Toggle aria-controls="basic-navbar-nav" />
                       <Navbar.Collapse className="justify-content-end mx-2" id="basic-navbar-nav">
                         <Nav className="">
